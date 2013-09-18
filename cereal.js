03edb2b918ee40f0d00df3f73d538a91de571072
pmallory <philip@pmallory.com>
2013-09-17 21:27:03 -0400

    filter with parallel coordinates diagram

--- cereal.js
+++ cereal.js
@@ -20,14 +20,59 @@ $(document).ready(function() {
 
 });
 
-function filter(values) {
+function shelf_filter(values) {
+    if (!values["Calories"]) {
+        values["Calories"] = new Object();
+        values["Calories"]["min"] = -1;
+        values["Calories"]["max"] = 99999;
+    }
+    if (!values["Protein (g)"]) {
+        values["Protein (g)"] = new Object();
+        values["Protein (g)"]["min"] = -1;
+        values["Protein (g)"]["max"] = 99999;
+    }
+    if (!values["Fat (g)"]) {
+        values["Fat (g)"] = new Object();
+        values["Fat (g)"]["min"] = -1;
+        values["Fat (g)"]["max"] = 99999;
+    }
+    if (!values["Sodium (mg)"]) {
+        values["Sodium (mg)"] = new Object();
+        values["Sodium (mg)"]["min"] = -1;
+        values["Sodium (mg)"]["max"] = 99999;
+    }
+    if (!values["Fiber (g)"]) {
+        values["Fiber (g)"] = new Object();
+        values["Fiber (g)"]["min"] = -1;
+        values["Fiber (g)"]["max"] = 99999;
+    }
+    if (!values["Carbohydrates (g)"]) {
+        values["Carbohydrates (g)"] = new Object();
+        values["Carbohydrates (g)"]["min"] = -1;
+        values["Carbohydrates (g)"]["max"] = 99999;
+    }
+    if (!values["Sugars (g)"]) {
+        values["Sugars (g)"] = new Object();
+        values["Sugars (g)"]["min"] = -1;
+        values["Sugars (g)"]["max"] = 99999;
+    }
+
     console.log(values);
-    d3.csv("DATA_Cereal_Revised.csv", function(csv){
+
+    d3.csv("cereal.csv", function(csv){
         for (var i=0; i<csv.length; i++) {
-            if ((csv[i].calories <= values[0]) || (csv[i].calories >= values[1])) {
-                $('#'+csv[i].name).hide();
-            } else {
+            if (
+               ((csv[i]["Calories"] >= values["Calories"]["min"]) && (csv[i]["Calories"] <= values["Calories"]["max"])) &&
+               ((csv[i]["Protein (g)"] >= values["Protein (g)"]["min"]) && (csv[i]["Protein (g)"] <= values["Protein (g)"]["max"])) &&
+               ((csv[i]["Fat (g)"] >= values["Fat (g)"]["min"]) && (csv[i]["Fat (g)"] <= values["Fat (g)"]["max"])) &&
+               ((csv[i]["Sodium (mg)"] >= values["Sodium (mg)"]["min"]) && (csv[i]["Sodium (mg)"] <= values["Sodium (mg)"]["max"])) &&
+               ((csv[i]["Fiber (g)"] >= values["Fiber (g)"]["min"]) && (csv[i]["Fiber (g)"] <= values["Fiber (g)"]["max"])) &&
+               ((csv[i]["Carbohydrates (g)"] >= values["Carbohydrates (g)"]["min"]) && (csv[i]["Carbohydrates (g)"] <= values["Carbohydrates (g)"]["max"])) &&
+               ((csv[i]["Sugars (g)"] >= values["Sugars (g)"]["min"]) && (csv[i]["Sugars (g)"] <= values["Sugars (g)"]["max"]))
+            ) {
                 $('#'+csv[i].name).show();
+            } else {
+                $('#'+csv[i].name).hide();
             }
         }
     });
